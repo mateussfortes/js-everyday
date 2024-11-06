bb2b627bd0688472f81fb00801a639aedd141f8b
= <=>
2024-11-05 23:33:38 -0300

    First commit

---
+++ 01-introduction.js
@@ -0,0 +1,14 @@
+// javascript can change html content
+document.getElementById("demo").innerHTML = "Hello Javascript"
+
+// javascript accepts both double and single quotes
+document.getElementById('demo').innerHTML = 'Hello Javascript'
+
+// javascript can change html styeles (css)
+document.getElementById('demo').style.fontSize = "35px"
+
+// javascript can hide html elements
+document.getElementById("demo").style.display = "none"
+
+// javascript can show html elements
+document.getElementById("demo").style.display = "block"
