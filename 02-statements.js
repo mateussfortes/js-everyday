788973b90ddf7abb927bf98ee7a74c5763527060
= <=>
2024-11-08 11:21:10 -0300

    Statements

---
+++ 02-statements.js
@@ -0,0 +1,24 @@
+let a, b, c; // delcare 3 variables
+a = 5; // assign value 5 to a
+b = 6; // assign value 6 to b
+c = a + b; // assign the sum of and b to c
+
+// javascript ignore whitespaces
+let person = "Hege";
+
+// good pratice is put space beteween operators + - * /
+let x = y + z;
+
+/*
+    For best read ability avoid code lines longer than 80
+    Break after an operator
+*/
+document.getElementById("cart").innerHTML =
+"Hello Dolly!";
+
+
+// code blocks
+function myFunction() {
+    document.getElementById("demo1").innerHTML = "Hello Dolly!";
+    document.getElementById("demo2").innerHTML = "Hello World!";
+}
