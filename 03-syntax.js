9ac393c01ca0887692481a35a890453cbc02ccf8
= <=>
2024-11-10 21:19:55 -0300

    Syntax

---
+++ 03-syntax.js
@@ -0,0 +1,72 @@
+// javascript literals 
+
+// numbers
+10.50
+1001
+
+//text
+"Jhon Doe"
+
+// variables
+    // var, let and const
+
+let x;
+x = 6;
+
+// javascript operators
+(5 + 6) * 8;
+
+let x, y;
+x = 10;
+y = 20;
+
+
+// javascript expressions
+
+5 * 10;
+x * 10;
+"Jhon" + " " + "Doe";
+
+
+// keywords
+
+let x, y;
+x = 5 + 6;
+y = x * 10
+
+
+// comments
+
+let x = 5;
+
+// x = 6 it will not be executed
+
+
+// identifiers
+    // a letter a-z AZ
+    // dollar sign $
+    // a underscore _
+
+// Numbers are not allowed as the first character in names
+
+
+// Case Sensitive
+    // lastName and lastname are two diferent variables
+
+let lastName, lastname;
+lastName = "Doe";
+lastname = "Jhon";
+
+
+// Camel Case
+    // Hyphens are no allowed first-name
+
+// Underscore
+let fist_name, last_nane;
+
+// Upper Camel Case
+let FirstName, LastName;
+
+// Lower Camel Case (JS most use)
+let firstName, lastName;
+
