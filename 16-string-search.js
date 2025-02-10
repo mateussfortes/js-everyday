f21c98f7a8d7915de46b62a481ef3d0e66c4ae5a
Mateus <mateussilveirafortes@gmail.com>
2025-02-10 15:31:15 -0300

    Finish string search

--- 16-string-search.js
+++ 16-string-search.js
@@ -26,6 +26,31 @@ let index2 = text2.search("locate");
 
 // Cannot take a second start position argument
 
+// match
+/*
+    Returns an array containig the results of matching a string against a string
+*/
+let text3 = "The rain in SPAIN stays mainly in the plain";
+text.match("ain");
+
+// match all
+// returns an iterator of all matches
 
 
+// includes
+let text4 = "hello world, welcome to the universe";
+text.includes("world"); // return true
+// starts at position 12
+text.includes("world", 12); // false
+// is case sensititve
+// es6
 
+// starts with
+// return true if a string begins with a specified value
+let text5 = "Hello world, welcome to the universe";
+text5.startsWith("hello");
+// is case sensitive
+// es6
+// second parameter is where position start ("hello", 12)
+
+// ends with, the same as starts with
